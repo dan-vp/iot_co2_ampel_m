46569857efdefb234c121ca796bd821cc9630b96
av1c3nna <73712507+av1c3nna@users.noreply.github.com>
2024-05-06 12:43:57 +0200

    created preprocessing class to handle invalid values, outliers, creation of new features etc.

--- Preprocessing_M.py
+++ Preprocessing_M.py
@@ -122,3 +122,164 @@ class Data_Extractor:
             print(round(time.time() - t, 2))
 
         return df
+    
+
+
+
+
+    class Data_Preprocessing:
+
+        def __init__(self, get_outliers_out = True):
+            self.get_outliers_out = get_outliers_out
+
+
+        def preprocess_df(self, df):
+            """Overall method to perform the preprocessing steps for a given dataframe df."""
+            
+            df = self.convert_features(df)
+            df = self.create_new_features(df)
+            df = self.remove_duplicates(df)
+            df = self.remove_features(df)
+            df = self.remove_invalid_values(df)
+            if self.get_outliers_out:
+                df = self.remove_outliers(df)
+            df = self.create_average_differentials(df)
+
+            df = self.fill_na(df)
+
+            df = df.sort_values(["date_time"])
+            # reset the index, since it is only int values. They should have a logical order.
+            df = df.reset_index(drop = True)
+
+            return df
+        
+        
+        def remove_duplicates(self, df):
+            # remove duplicated data points
+            df = df.drop_duplicates(["date_time", "room_number"])
+            return df
+        
+
+        def fill_na(self, df):
+            """Handle missing values in the data."""
+
+            for col in df.columns:
+                is_na = df[col].isna().any()
+                if is_na:
+                    if "_diff" in col:
+                        df[col] = df[col].fillna(0)
+                    else:
+                        df[col] = df[col].fillna("bfill")
+                        df[col] = df[col].fillna("ffill")
+
+            return df
+
+
+        def convert_features(self, df):
+            """Convert the formats of features to their correct ones."""
+
+            df["date_time"] = pd.to_datetime(df["date_time"])
+            df["snr"] = df["snr"].astype("float")
+            return df
+
+
+        def remove_features(self, df):
+            """Remove redundant features."""
+
+            df.drop(columns = ["WIFI", "bandwidth", "channel_rssi", "channel_index", "device_id", "gateway", "f_cnt", "spreading_factor"], axis = 1, inplace = True)
+            
+            return df
+
+
+        def remove_invalid_values(self, df):
+            """Remove invalid data points from the dataset."""
+
+            # humidity describes a percent value. Therefore, values over 100 are invalid
+            df = df[df.hum <= 100]
+            # temperature is measured in °C. Therefore, we assume there can't be room temperatures above 50°C.
+            # this filter method might have to be reevaluated in case of fire outbreaks
+            df = df[df.tmp <= 50]
+
+            # The dataset contains data points with multiple zeroes. We assume they could result from false reading or resets of the sensor devices.
+            df = df[(df.CO2 != 0) & (df.VOC != 0) & (df.tmp != 0) & (df.hum != 0)]
+            # Thousands of data points include high inclines of the CO2 value while the other measured variables freeze (e.g. in some of them VOC stays at 450)
+            invalid_values = (df.CO2 > 20000) & (df.VOC_diff == 0) & (df.VOC_diff == 0) & (df.BLE_diff == 0) & (df.tmp_diff == 0)
+            df = df[invalid_values == False]
+
+            df.reset_index(drop = True, inplace = True)
+
+            return df
+
+
+        def create_new_features(self, df):
+            """Create new features for the Machine Learning training phase."""
+
+            try:
+                # according to https://www.h-ka.de/fileadmin/Hochschule_Karlsruhe_HKA/Bilder_VW-EBI/HKA_VW-EBI_Anleitung_CO2-Ampeln.pdf
+                # due to simplicity, we treat every value under 850 as green, as the CO2 value declines from 850 to 700 rapidly anyway.
+                df.loc[(df.CO2 < 850), "color"] = "green"
+                df.loc[(df.CO2 >= 850) & (df.CO2 < 1200), "color"] = "yellow"
+                df.loc[(df.CO2 >= 1200) & (df.CO2 < 1600), "color"] = "red"
+                df.loc[(df.CO2 >= 1600), "color"] = "red_blinking"
+
+                # we treat the year 2022 as 1, year 2023 as 2 etc.
+                df["year"] = df.date_time.dt.year - 2021
+
+                df.loc[:, "room_number"] = df["device_id"].str.split("-").str[-1]
+                df.loc[:, "building_name"] = df["room_number"].str.replace('\d+', '', regex = True)
+
+                building_dict = {"ama":"am","amb":"am", 
+                                    "ba":"b", "bb":"b",
+                                    "eu":"e",
+                                    "fa":"f","fu":"f",
+                                    "lia":"li","lib":"li","lie":"li","liu":"li",
+                                    "mu":"m"}
+                
+                df.replace({"building_name": building_dict}, inplace = True)
+            
+            except Exception as e:
+                print(e)
+                pass
+
+            df.sort_values(by = ["date_time"]).reset_index(drop = True, inplace = True)
+
+            df["time_diff_sec"] = df.groupby('room_number')["date_time"].diff().dt.seconds
+
+            # Iterate over each group
+            # create new features which show the value changes compared to the previous data point
+            for feature in ["tmp", "hum", "CO2", "VOC", "vis", "IR", "BLE", "vis"]:
+                    df[f"{feature}_diff"] = df.groupby('room_number')[feature].diff()
+
+            df.fillna(0, inplace = True)
+
+            df["dayofweek"] = df["date_time"].dt.dayofweek
+            df["hour"] = df["date_time"].dt.hour
+            df["month"] = df["date_time"].dt.month
+
+
+            return df
+
+
+        def remove_outliers(self, df):
+                """Remove outliers for CO2, VOC, tmp and hum."""
+
+                for feature in ["CO2_diff", "VOC_diff", "tmp_diff", "hum_diff"]:
+                        threshold = df[feature].mean() + 4*df[feature].std()
+                        df = df[df[feature].abs() <= threshold]
+
+                df.reset_index(drop = True, inplace = True)
+
+                # since outliers (data points) were removed, the measured differences of variable changes between time points of a specific room are not accurate. They have to be calculated again.
+                # The differences were required to estimate invalid values or outliers. Thus it has been implemented this way.
+                df = self.create_new_features(df)
+
+                return df
+
+        
+        def create_average_differentials(self, df):
+            """Calculate the average differentials per second for CO2, VOC, tmp, hum, IR and vis."""
+
+            for feature in ["CO2_diff", "VOC_diff", "tmp_diff", "hum_diff", "IR_diff", "vis_diff"]:
+                    df[f"{feature}_per_sec"] = df[feature].div(df["time_diff_sec"])
+
+            return df
