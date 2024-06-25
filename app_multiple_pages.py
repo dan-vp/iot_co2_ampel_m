a9fe0bdbb637ae416110fa05798931824f021d49
Matrox05 <max.dachtler@t-online.de>
2024-06-25 14:43:09 +0200

    added code documentation

--- app_multiple_pages.py
+++ app_multiple_pages.py
@@ -1,6 +1,5 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html, dcc
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import pandas as pd
@@ -10,49 +9,38 @@ import tensorflow as tf
 from Evaluator import Evaluator
 import plotly.express as px
 from ML_Deployment import *
-import dash_table
+from dash import dash_table
 
 # Load data
-df = pd.read_parquet("../Gebäude_M_Analysis/Preprocessed_M_Data.parquet")
-df_forecast = pd.read_parquet("df_forecast.parquet")
-df_raw = pd.read_parquet("df_raw.parquet")
+df = pd.read_parquet("../Gebäude_M_Analysis/Preprocessed_M_Data.parquet")   # dataframe with no rolling window and no sampling greater than 1 hour
+df_forecast = pd.read_parquet("df_forecast.parquet")    # dataframewith rolling window 3d and sample time 1d
+df_raw = pd.read_parquet("df_raw.parquet")  #raw dataframe without any preprocessing
 
 
 df["date"] = df["date_time"].dt.date
 df["hour"] = df["date_time"].dt.hour
 
-wochentage_deutsch = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"]
-
-df["dayofweekstr"] = [wochentage_deutsch[i] for i in df["dayofweek"]]
-
-#daily_counts = df.groupby('date').size().reset_index(name='count')
-
 sensor_data_liste = ['tmp', 'hum', 'CO2', 'VOC', 'vis', 'IR', 'BLE']
 room_numbers = df['room_number'].unique()
-room_numbers_forecast = ["m209"]
 n_steps = 7
 
-print("läuft 1")
+# loading Forecast
 fe = FeatureEngineering(df_forecast,
                         label = "CO2", 
                         categorical_features = ["season", "room_number", "dayofweek"],
                         automated_feature_engineering = False)
-print("läuft 2")
 X_train, X_val, X_test, y_train, y_val, y_test = fe.feature_engineering(steps_to_forecast = n_steps, skip_scale = True)
-print("läuft 3")
 model = tf.keras.models.load_model(f"CO2_Forecasting_Model.keras")
-print("läuft 4")
 pred = model.predict(fe.X_test)
-print("läuft 5")
 fe.df = df_raw.copy()
-print("läuft 6")
 deployer = Predictor(data = df_raw, feature_engineering_class_object = fe, label = "CO2", is_forecast = True, roll = True, steps_to_forecast = 2)
-print("läuft 7")
 forecasted_pred = deployer.predict(x = deployer.x, model = model)
 forecasted_pred.reset_index(inplace=True)
 forecasted_pred = forecasted_pred[forecasted_pred["date_time"] == df_forecast.date_time.max()].sort_values("room_number")
 
 ev = Evaluator()
+
+#plotting forecast
 
 fig_forecast = px.line(y = [fe.y_test[:, 0], pred[:, 0]], 
             labels = {"wide_variable_0": "y_true",
@@ -77,7 +65,6 @@ def get_options(sensor_data):
 
 options = get_options(sensor_data_liste)
 room_options = [{'label': 'Keine Filterung', 'value': 'all'}] + get_options(room_numbers)
-room_options_forecast = get_options(room_numbers_forecast)
 # Layout for the sidebar
 sidebar_layout = html.Div(
     children=[
@@ -116,12 +103,13 @@ def display_page(pathname):
     else:
         return uebersicht_layout
 
+# Layout uebersicht page
 uebersicht_layout = html.Div(
     children=[
         html.H1('Übersicht'),
         html.Div(
             children=[
-                html.P('Wähle Filteroptionen:'),
+                html.P('Wähle räumliche Filteroptionen:'),
                 dcc.Dropdown(id='count-filter-selector', options=room_options,
                              multi=True,
                              style={'backgroundColor': '#1E1E1E', 'width': '80%', 'display': 'inline-block'},
@@ -146,7 +134,7 @@ monatsuebersicht_layout = html.Div(
                              className='month-sensordataselector'
                 ),
                 html.Div(id='month-input-warning', style={'display': 'inline-block', 'width': '4%'}),
-                html.P('Wähle Filteroptionen:'),
+                html.P('Wähle räumliche Filteroptionen:'),
                 dcc.Dropdown(id='month-filter-selector', options=room_options,
                              multi=True, value=['all'],
                              style={'backgroundColor': '#1E1E1E', 'width': '80%', 'display': 'inline-block'},
@@ -172,7 +160,7 @@ tagesuebersicht_layout = html.Div(
                              className='sensordataselector'
                 ),
                 html.Div(id='input-warning', style={'display': 'inline-block', 'width': '4%'}),
-                html.P('Wähle Filteroptionen:'),
+                html.P('Wähle räumliche Filteroptionen:'),
                 dcc.Dropdown(id='filter-selector', options=room_options,
                              multi=True, value=['all'],
                              style={'backgroundColor': '#1E1E1E', 'width': '48%', 'display': 'inline-block'},
@@ -191,6 +179,7 @@ tagesuebersicht_layout = html.Div(
     ]
 )
 
+#Layout for forecast page
 forecast_layout = html.Div(
     children=[
         html.H1('Forecast'),
@@ -216,45 +205,39 @@ forecast_layout = html.Div(
     [Input("sensordataselector", "value")]
 )
 def update_filter_options(sensordata):
-    """Aktualisiert die Filteroptionen basierend auf der Anzahl der ausgewählten Sensordaten"""
-
-    print(sensordata)
+    """update filteroptions, depending on what is selected"""
 
     warning = None
     room_disabled = False
     multi_select = True
 
-    # Wenn zwei Sensordaten ausgewählt sind, zeige Warnung und setze die Filter zurück
+    # only allow 2 sensor data choices
     if len(sensordata) > 1:
-        #warning = html.P("Bei Auswahl von 2 Sensordaten wird standardmäßig 'Keine Filterung' angezeigt. Eine einzelne Filterung kann ausgewählt werden.", id='input-warning')
-        room_disabled = False  # Erlaube die Auswahl im Dropdown
-        multi_select = False  # Erlaube nur eine einzelne Auswahl
-        selected_value = 'all'  # Setze auf 'Keine Filterung'
+        room_disabled = False  # allow choice   
+        multi_select = False  # allow multi select  
+        selected_value = 'all'  # choose 'all' as default value
     else:
-        selected_value = ['all']  # Standardwert, wenn weniger als zwei Sensordaten ausgewählt sind
-    print(warning, room_options, selected_value, room_disabled, multi_select)
+        selected_value = ['all']  # default value
     return [warning, room_options, selected_value, room_disabled, multi_select]
 
 @app.callback(Output('timeseries', 'figure'),
               [Input('filter-selector', 'value'),
                Input('sensordataselector', 'value')])
 def update_timeseries(selected_filters, selected_dropdown_value):
-    print(selected_filters, selected_dropdown_value)
-    ''' Zeichnet die Verläufe der ausgewählten Sensordaten basierend auf dem aktuell ausgewählten Filter '''
+    ''' lineplot for daily aggregation '''
     if len(selected_dropdown_value) > 2:
         selected_dropdown_value = selected_dropdown_value[:2]
 
-    # Überprüfen, ob die ausgewählten Filter eine Liste sind, wenn nicht, in eine Liste umwandeln
+    # convert to list if neccessary
     if not isinstance(selected_filters, list):
         selected_filters = [selected_filters]
 
-    # Erstellen Sie das Figure-Objekt
+    # create fig object
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    # Überprüfen, ob "all" in den ausgewählten Filtern ist, und fügen Sie entsprechende Traces hinzu
+    # check if all is in selected filter, so that traces can be combined
     if 'all' in selected_filters:
         for data in selected_dropdown_value:
-            print("fig wird geupdated 1")
             data_daily_all = df.groupby('hour', as_index=False).agg({data: "mean"})
             fig.add_trace(
                 go.Scatter(x=data_daily_all["hour"],
@@ -265,16 +248,14 @@ def update_timeseries(selected_filters, selected_dropdown_value):
                            textposition='bottom center'),
                 secondary_y=False if data == selected_dropdown_value[0] else True)
 
-    # Fügen Sie Traces für die ausgewählten Räume hinzu
+    # add traces for room numbers
     
     for room in selected_filters:
         room_data = df[df['room_number'] == room]
         for data in selected_dropdown_value:
             room_daily = room_data.groupby('hour', as_index=False).agg({data: "mean"})
-            # Setzen Sie den Namen des Traces basierend auf dem spezifischen Raum
+            # specific name for every trace
             trace_name = f'{data} (alle Räume)' if room == 'all' else f'{data} (Raum {room})'
-            print("fig wird geupdated 2")
-            print(trace_name)
             fig.add_trace(
                 go.Scatter(x=room_daily["hour"],
                         y=room_daily[data],
@@ -283,10 +264,8 @@ def update_timeseries(selected_filters, selected_dropdown_value):
                         name=trace_name,
                         textposition='bottom center'),
                 secondary_y=False if data == selected_dropdown_value[0] else True)
-                
-
-    print("fig wird geupdatet 3")
-    # Aktualisieren Sie das Layout des Plots
+            
+    # update layout
     fig.update_layout(
         colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
         template='plotly_dark',
@@ -317,7 +296,7 @@ def update_daily_timeseries(selected_date, selected_filters, selected_dropdown_v
     except ValueError:
         return go.Figure()  # Return an empty figure if the date is invalid
 
-    # Check if the selected filters are a list, if not, convert them to a list
+    # convert to list, if neccessary
     if not isinstance(selected_filters, list):
         selected_filters = [selected_filters]
 
@@ -375,19 +354,19 @@ def update_daily_timeseries(selected_date, selected_filters, selected_dropdown_v
     [Input("month-sensordataselector", "value")]
 )
 def update_monthly_filter_options(sensordata):
-    """Aktualisiert die Filteroptionen basierend auf der Anzahl der ausgewählten Sensordaten"""
+    """updating filteroptions depending on what is selected"""
     warning = None
     room_disabled = False
     multi_select = True
 
-    # Wenn zwei Sensordaten ausgewählt sind, zeige Warnung und setze die Filter zurück
+    
     if len(sensordata) > 1:
-        #warning = html.P("Bei Auswahl von 2 Sensordaten wird standardmäßig 'Keine Filterung' angezeigt. Eine einzelne Filterung kann ausgewählt werden.", id='input-warning')
-        room_disabled = False  # Erlaube die Auswahl im Dropdown
-        multi_select = False  # Erlaube nur eine einzelne Auswahl
-        selected_value = 'all'  # Setze auf 'Keine Filterung'
+
+        room_disabled = False  
+        multi_select = False  
+        selected_value = 'all'  
     else:
-        selected_value = ['all']  # Standardwert, wenn weniger als zwei Sensordaten ausgewählt sind
+        selected_value = ['all']  
 
     return [warning, room_options, selected_value, room_disabled, multi_select]
 
@@ -396,18 +375,17 @@ def update_monthly_filter_options(sensordata):
                Input('month-sensordataselector', 'value')])
 
 def update_monthly_timeseries(selected_filters, selected_dropdown_value):
-    ''' Zeichnet die Verläufe der ausgewählten Sensordaten basierend auf dem aktuell ausgewählten Filter '''
+    ''' lineplot group by date '''
     if len(selected_dropdown_value) > 2:
         selected_dropdown_value = selected_dropdown_value[:2]
 
-    # Überprüfen, ob die ausgewählten Filter eine Liste sind, wenn nicht, in eine Liste umwandeln
     if not isinstance(selected_filters, list):
         selected_filters = [selected_filters]
 
-    # Erstellen Sie das Figure-Objekt
+    # create fig object
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    # Überprüfen, ob "all" in den ausgewählten Filtern ist, und fügen Sie entsprechende Traces hinzu
+    # add 'all' trace
     if 'all' in selected_filters:
         for data in selected_dropdown_value:
             data_daily_all = df.groupby('date', as_index=False).agg({data: "mean"})
@@ -420,13 +398,13 @@ def update_monthly_timeseries(selected_filters, selected_dropdown_value):
                            textposition='bottom center'),
                 secondary_y=False if data == selected_dropdown_value[0] else True)
 
-    # Fügen Sie Traces für die ausgewählten Räume hinzu
+    # add traces for rooms
     
     for room in selected_filters:
         room_data = df[df['room_number'] == room]
         for data in selected_dropdown_value:
             room_daily = room_data.groupby('date', as_index=False).agg({data: "mean"})
-            # Setzen Sie den Namen des Traces basierend auf dem spezifischen Raum
+            
             trace_name = f'{data} (alle Räume)' if room == 'all' else f'{data} (Raum {room})'
             fig.add_trace(
                 go.Scatter(x=room_daily["date"],
@@ -438,7 +416,7 @@ def update_monthly_timeseries(selected_filters, selected_dropdown_value):
                 secondary_y=False if data == selected_dropdown_value[0] else True)
                 
 
-    # Aktualisieren Sie das Layout des Plots
+    # update plot
     fig.update_layout(
         colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
         template='plotly_dark',
@@ -456,31 +434,27 @@ def update_monthly_timeseries(selected_filters, selected_dropdown_value):
               [Input('count-filter-selector', 'value')])
 
 def update_count_timeseries(selected_filters):
-    ''' Zeichnet die Verläufe der ausgewählten Sensordaten basierend auf dem aktuell ausgewählten Filter '''
+    ''' histogram per date '''
 
-    # Überprüfen, ob die ausgewählten Filter eine Liste sind, wenn nicht, in eine Liste umwandeln
+    
     if not isinstance(selected_filters, list):
         selected_filters = [selected_filters]
 
-    # Erstellen Sie das Figure-Objekt
+    # create fig object
     fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-    # Überprüfen, ob "all" in den ausgewählten Filtern ist, und fügen Sie entsprechende Traces hinzu
+    # add trace for 'all'
     if 'all' in selected_filters:
-            #data_daily_all = df.groupby('date', as_index=False).agg({data: "mean"})
          fig.add_trace(
             go.Histogram(x=df["date"],
                         opacity=0.7,
-                        nbinsx=50,
                         name=f'count (alle Räume)'))
 
-    #Fügen Sie Traces für die ausgewählten Räume hinzu
+    #add traces for rooms
     
     for room in selected_filters:
         if room != 'all':
             room_data = df[df['room_number'] == room]
-                #room_daily = room_data.groupby('date', as_index=False).agg({data: "mean"})
-                # Setzen Sie den Namen des Traces basierend auf dem spezifischen Raum
             trace_name = f'count (alle Räume)' if room == 'all' else f'count (Raum {room})'
             fig.add_trace(
                 go.Histogram(x=room_data["date"],
@@ -488,7 +462,7 @@ def update_count_timeseries(selected_filters):
                         name=trace_name))
                 
 
-    # Aktualisieren Sie das Layout des Plots
+    # update layout of plot
     fig.update_layout(
         colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
         template='plotly_dark',
